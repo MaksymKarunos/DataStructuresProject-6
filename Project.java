522159786e9bd57b2a1ed0f5bf37d5a5cf08bb3b
Maksym Karunos <maksymkarunos@Maksyms-MacBook-Pro.local>
2019-11-28 16:34:14 -0500

    Happy Thanksgiving

---
+++ Project.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Project{
+    public static void main(String[] args){
+        Scanner myScanner = new Scanner(System.in);
+        System.out.println("Input n");
+        int n = myScanner.nextInt();
+        myScanner.close();
+        // PLEASE LEAEN 
+        // I love new jersey
+
+    }
+}
