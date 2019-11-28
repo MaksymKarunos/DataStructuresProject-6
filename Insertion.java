ed9a2aae75bd3205c656c86d541f64bcaee7ce90
Maksym Karunos <maksymkarunos@Maksyms-MacBook-Pro.local>
2019-11-28 18:05:51 -0500

    Main + Insertion updated

--- Insertion.java
+++ Insertion.java
@@ -1,9 +1,15 @@
 public class Insertion{
-    void InsertionSort(int[] arr){
-        int n = arr.length;
-        for ( int i = 0; i < n; i++){
-        int firstUnsorted = arr[i];
-        int j = i - 1; // first element to compare to
-        }
-    }
+    static void sort(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 1; i < n; ++i) { 
+            int key = arr[i]; 
+            int j = i - 1; 
+            while (j >= 0 && arr[j] > key) { 
+                arr[j + 1] = arr[j]; 
+                j = j - 1; 
+            } 
+            arr[j + 1] = key; 
+        } 
+    } 
 }
--- Project.java
+++ Project.java
@@ -1,10 +1,36 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Project{
+    static void printArray(int arr[]) 
+    { 
+        int n = arr.length; 
+        for (int i = 0; i < n; ++i) 
+            System.out.print(arr[i] + " "); 
+  
+        System.out.println(); 
+    } 
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);
+        Random myRandom = new Random(228);
         System.out.println("Input n");
         int n = myScanner.nextInt();
+
+        int[] decendingOrder = new int[n];
+        int[] ascedningOrder = new int[n];
+        int[] randomOrder = new int[n];
+
+        for ( int i = 0; i < n; i++ ){
+            ascedningOrder[i] = i;
+        }
+        for ( int i = n-1; i > 0; i--){
+            decendingOrder[i] = i;
+        }
+        for ( int i = 0; i < n; i++){
+            randomOrder[i] = myRandom.nextInt(Integer.MAX_VALUE);
+        }
+        Insertion.sort(randomOrder);
+        Project.printArray(randomOrder);
         myScanner.close();
         // PLEASE LEAEN 
         // I love new jersey
