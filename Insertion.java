e7299a5f56dee8ec1b8def8f6be7741ef29475fc
Maksym Karunos <maksymkarunos@Maksyms-MacBook-Pro.local>
2019-11-28 17:20:14 -0500

    Path Update

---
+++ Insertion.java
@@ -0,0 +1,9 @@
+public class Insertion{
+    void InsertionSort(int[] arr){
+        int n = arr.length;
+        for ( int i = 0; i < n; i++){
+        int firstUnsorted = arr[i];
+        int j = i - 1; // first element to compare to
+        }
+    }
+}
